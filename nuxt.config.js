8c6f18873ad831290cd8653473d271970fcb645b
suzukuF <1226747845@qq.com>
2020-07-23 16:41:56 +0800

    修改首页轮播为iview框架

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const isDev = process.env.NODE_ENV === 'development'
+// const isDev = process.env.NODE_ENV === 'development'
 // const target = isDev ? 'http://192.168.1.4:81' : 'http://haofang.wkan.cn'
 const target = 'http://admin.azhfang.com'
 
